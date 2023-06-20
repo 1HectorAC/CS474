1d33b863c8fee8641fbb51a655fab5da200294bf
Hector Acosta <1Hector.AC@gmail.com>
2023-06-20 12:06:41 -0700

    Edit results display in lab4.

--- cs474Lab4/cs474Lab4/Program.cs
+++ cs474Lab4/cs474Lab4/Program.cs
@@ -7,13 +7,9 @@ namespace cs474Lab4
 {
     class Program
     {
-        const int sectionSize = 20000000;
+        private static readonly int CC = Environment.ProcessorCount;
 
-        private static int CC = Environment.ProcessorCount;
-
-        private static readonly int Chunk = sectionSize / CC;
-
-        //This will get the height value of of a specified x value location.
+        //This will get the height value of a specified x value location.
         static double GetHeight(double location) =>  (4 / (1 + (location * location)));
 
         //This will get the X position inbetween given a section size and n.
@@ -22,38 +18,38 @@ namespace cs474Lab4
         /// <summary>
         /// This method will sequentially calculate the area under the curve.
         /// </summary>
-        /// <returns>The total value</returns>
-        static double SequentialAreaUnderSum()
+        /// <returns>The total value.</returns>
+        static double SequentialAreaUnderSum(int size)
         {
             double total = 0;
 
             //loops through every section and adds rectangles.
-            for (int i = 0; i < sectionSize; i++)
-                total += (GetHeight(GetXValue(i, 1.0 * 1 / sectionSize)) * 1 / sectionSize);
+            for (int i = 0; i < size; i++)
+                total += (GetHeight(GetXValue(i, 1.0 * 1 / size)) * 1 / size);
             return total;
         }
 
         /// <summary>
         /// This method will parallelly calculate the area under the curve.
         /// </summary>
-        /// <returns>The total value</returns>
-        static double ParallelAreaUnderSum()
+        /// <returns>The total value.</returns>
+        static double ParallelAreaUnderSum(int size, int chunk)
         {
             double total = 0;
             Mutex mLock = new Mutex();
 
             //loops through every section and adds rectangles.
-            Parallel.For(0, Convert.ToInt32(Math.Ceiling(1.0 * sectionSize / Chunk)), j =>
+            Parallel.For(0, Convert.ToInt32(Math.Ceiling(1.0 * size / chunk)), j =>
             {
-                int start = j * Chunk;
-                int end = (j + 1) * Chunk;
+                int start = j * chunk;
+                int end = (j + 1) * chunk;
                 double localTotal = 0;
                 for (int i = start; i < end; i++)
                 {
                     //Break if we go over the specified length. (More loops than are sections)
-                    if (i >= sectionSize)
+                    if (i >= size)
                         break;
-                    localTotal += (GetHeight(GetXValue(i, 1.0 * 1 / sectionSize)) * 1 / sectionSize);
+                    localTotal += (GetHeight(GetXValue(i, 1.0 * 1 / size)) * 1 / size);
                 }
                 mLock.WaitOne();
                 total += localTotal;
@@ -61,25 +57,35 @@ namespace cs474Lab4
             });
             return total;
         }
+        static bool CompareCheck(double val1, double val2) => val1 == val2;
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Area Under Curve Algorithm Comparison");
-            Console.WriteLine($"Section Size: {sectionSize}\nChunk Size: {Chunk}\n");
-
             Stopwatch stopWatch = new Stopwatch();
-            Console.WriteLine("Sequential Algorithm:");
-            stopWatch.Start();
-            double result1 = SequentialAreaUnderSum();
-            stopWatch.Stop();
-            Console.WriteLine($"Result: {result1}\nTime: {stopWatch.ElapsedMilliseconds}ms\n");
+            
+            int[] sizes = {100, 1000, 1000000, 2000000 };
 
-            stopWatch.Reset();
-            Console.WriteLine("Parallel Algorithm:");
-            stopWatch.Start();
-            double result2 = ParallelAreaUnderSum();
-            stopWatch.Stop();
-            Console.WriteLine($"Result: {result2}\nTime: {stopWatch.ElapsedMilliseconds}ms\n");
+            Console.WriteLine("Area Under Curve Algorithm Comparison");
+            Console.WriteLine($"Processors:{CC}\n");
+
+            for(int i = 0; i < sizes.Length; i++)
+            {
+                int chunkSize = sizes[i] / CC;
+                Console.WriteLine($"Size: {sizes[i]}\nChunk: {chunkSize}");
+
+                stopWatch.Start();
+                double result1 = SequentialAreaUnderSum(sizes[i]);
+                stopWatch.Stop();
+                Console.WriteLine($"Sequential Algorithm Time: {stopWatch.ElapsedMilliseconds}ms");
+                stopWatch.Reset();
+
+                stopWatch.Start();
+                double result2 = ParallelAreaUnderSum(sizes[i],chunkSize);
+                stopWatch.Stop();
+                Console.WriteLine($"Parallel Algorithm Time: {stopWatch.ElapsedMilliseconds}ms");
+                stopWatch.Reset();
+                Console.WriteLine($"Equal Check: {CompareCheck(Math.Round(result1), Math.Round(result2))}\n");
+            }  
         }
     }
 }
