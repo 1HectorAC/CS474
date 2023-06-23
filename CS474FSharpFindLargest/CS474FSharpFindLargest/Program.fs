70872568ac04fb8641562f75cfec3e5b01cef5f3
Hector Acosta <1Hector.AC@gmail.com>
2023-06-23 15:14:25 -0700

    Refactored in CS474FSharpFindLargest project.

--- CS474FSharpFindLargest/CS474FSharpFindLargest/Program.fs
+++ CS474FSharpFindLargest/CS474FSharpFindLargest/Program.fs
@@ -4,7 +4,7 @@ open System.Threading.Tasks
 let CC = Environment.ProcessorCount
 
 //This function to get the largest number index of two numbers in array.
-let GetLargestBetweenTwoInArray (one,two: int, f: array<_>) = if f.[one] > f.[two] then one else two
+let GetLargestBetweenTwoInArray (one,two: int, f: array<_>) = if f[one] > f[two] then one else two
 
 //This function will check if the two numbers are equal
 let EqualCheck (one,two:int) = one = two
@@ -13,7 +13,7 @@ let EqualCheck (one,two:int) = one = two
 let SequentialLargestAlgorithm (arr:array<_>) =
     let mutable maxIndex = 0
     for i in 1.. arr.Length-1 do (maxIndex <- GetLargestBetweenTwoInArray(maxIndex, i,arr))
-    arr.[maxIndex]
+    arr[maxIndex]
 
 //Parallel Part.
 let ParallelLargestAlgorithm (arr:array<_>, chunk: int) = 
@@ -26,7 +26,7 @@ let ParallelLargestAlgorithm (arr:array<_>, chunk: int) =
         for i in start1.. (end1 - 1) do (localMax <- GetLargestBetweenTwoInArray(i,localMax,arr)) |> ignore
         lock (monitor) (fun () -> maxIndex <- GetLargestBetweenTwoInArray(localMax, maxIndex,arr)) |> ignore
          ) |> ignore
-    arr.[maxIndex]
+    arr[maxIndex]
 
 let main() =
     printfn "Find Largest Number Algorithm Comparison with F#"
@@ -41,7 +41,7 @@ let main() =
 
         //Setup array with random numbers.
         let arrayTest = Array.zeroCreate size
-        for i in 0.. (size-1) do arrayTest.[i] <- rnd.Next()
+        for i in 0.. (size-1) do arrayTest[i] <- rnd.Next()
 
         timer.Start()
         let sequentialLargest = SequentialLargestAlgorithm(arrayTest) 
